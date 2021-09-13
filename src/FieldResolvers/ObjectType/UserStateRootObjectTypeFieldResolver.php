93ea1f97d0a8f75a188c5ce0c592977384421608
leoloso <leo@getpop.org>
2021-09-13 08:58:10 +0000

    Merge pull request #1034 from leoloso/rename-fieldResolvers
    Renamed all `FieldResolvers` to `ObjectTypeFieldResolvers`

--- src/FieldResolvers/ObjectType/UserStateRootObjectTypeFieldResolver.php
+++ src/FieldResolvers/ObjectType/UserStateRootObjectTypeFieldResolver.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace PoPSchema\CommentMutations\FieldResolvers\ObjectType;
 
-use PoP\ComponentModel\FieldResolvers\ObjectType\AbstractQueryableFieldResolver;
+use PoP\ComponentModel\FieldResolvers\ObjectType\AbstractQueryableObjectTypeFieldResolver;
 use PoP\ComponentModel\FilterInput\FilterInputHelper;
 use PoP\ComponentModel\HelperServices\SemverHelperServiceInterface;
 use PoP\ComponentModel\Instances\InstanceManagerInterface;
@@ -27,11 +27,11 @@ use PoPSchema\SchemaCommons\DataLoading\ReturnTypes;
 use PoPSchema\SchemaCommons\FormInputs\OrderFormInput;
 use PoPSchema\SchemaCommons\ModuleProcessors\FormInputs\CommonFilterInputModuleProcessor;
 use PoPSchema\SchemaCommons\Resolvers\WithLimitFieldArgResolverTrait;
-use PoPSchema\UserState\FieldResolvers\ObjectType\UserStateFieldResolverTrait;
+use PoPSchema\UserState\FieldResolvers\ObjectType\UserStateObjectTypeFieldResolverTrait;
 
-class UserStateRootFieldResolver extends AbstractQueryableFieldResolver
+class UserStateRootObjectTypeFieldResolver extends AbstractQueryableObjectTypeFieldResolver
 {
-    use UserStateFieldResolverTrait;
+    use UserStateObjectTypeFieldResolverTrait;
     use WithLimitFieldArgResolverTrait;
 
     public function __construct(
